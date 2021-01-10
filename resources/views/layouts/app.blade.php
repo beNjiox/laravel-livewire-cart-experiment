8e57d0527204f47f5adae861a814aaf68b810dc4
Benjamin Guez <benjioxsz@gmail.com>
2021-01-10 17:33:53 +0100

    Prevent zoom on double tap mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <head>
     <title>Cart Experiment</title>
     <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     @livewireStyles
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
